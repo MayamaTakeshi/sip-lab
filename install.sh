f6cdd28a3947f760352b70af3ba36715da8327e7
MayamaTakeshi <mayamatakeshi@gmail.com>
2022-09-05 21:27:54 +0900

    #27: solved

--- install.sh
+++ install.sh
@@ -25,30 +25,6 @@ fi
 
 
 cd $START_DIR/3rdParty
-if [[ ! -d pjproject ]]
-then
-	git clone https://github.com/pjsip/pjproject
-	cd pjproject
-	#git checkout de3d744c2e1188b59bb907b6ee32ef83740ebc64
-    git checkout 33a3c9e0a5eb84426edef05a9aa98af17d8011c3 # required for bcg729
-
-    #echo "Patching sip_transaction.c to avoid problems with CANCEL"
-    sed -i -r 's|event->body.tx_msg.tdata == tsx->last_tx,|\t\t\t1, /* \0 */|' pjsip/src/pjsip/sip_transaction.c 
-
-	cat > user.mak <<EOF
-	export CFLAGS += -fPIC -g
-	export LDFLAGS +=
-EOF
-
-	./configure --with-bcg729
-	cat > pjlib/include/pj/config_site.h <<EOF
-#define PJMEDIA_HAS_SRTP  0
-EOF
-	make dep && make clean && make
-fi
-
-
-cd $START_DIR/3rdParty
 if [[ ! -d rapidjson ]]
 then
 	git clone https://github.com/Tencent/rapidjson
@@ -65,7 +41,35 @@ then
     git checkout faaa895862165acde6df8add722ba4f85a25007d
     cmake . 
     make
+    mkdir -p lib
+    cp -f src/libbcg729.a lib
 fi
+
+
+cd $START_DIR/3rdParty
+if [[ ! -d pjproject ]]
+then
+	git clone https://github.com/pjsip/pjproject
+	cd pjproject
+	#git checkout de3d744c2e1188b59bb907b6ee32ef83740ebc64
+    git checkout 33a3c9e0a5eb84426edef05a9aa98af17d8011c3 # required for bcg729
+
+    #echo "Patching sip_transaction.c to avoid problems with CANCEL"
+    sed -i -r 's|event->body.tx_msg.tdata == tsx->last_tx,|\t\t\t1, /* \0 */|' pjsip/src/pjsip/sip_transaction.c 
+
+	cat > user.mak <<EOF
+	export CFLAGS += -fPIC -g
+	export LDFLAGS +=
+EOF
+
+    sed -i -r 's/BCG729_LIBS="-lbcg729"/BCG729_LIBS=''/' aconfigure
+    LIBS=`pwd`/../bcg729/src/libbcg729.a ./configure --with-bcg729=`pwd`/../bcg729
+	cat > pjlib/include/pj/config_site.h <<EOF
+#define PJMEDIA_HAS_SRTP  0
+EOF
+	make dep && make clean && make
+fi
+
 
 
 cd $START_DIR
