fb65496b340c82e92a5075657e4162df5901fed1
MayamaTakeshi <mayamatakeshi@gmail.com>
2023-01-05 20:45:48 +0900

    #40 added basic tcp socket creation code (using pjlip activesock library). It passed smoke test

--- src/sip.cpp
+++ src/sip.cpp
@@ -676,6 +676,143 @@ static int get_media_mode(pjmedia_sdp_attr **attrs, int count) {
         return UNKNOWN;
 }
 
+
+
+// Adapted from
+/* 
+  https://www.pjsip.org/pjlib/docs/html/page_pjlib_ioqueue_tcp_test.htm
+  https://github.com/lroyd/zhangxy/blob/490748c745c99af147aeea18123dd15aac2d0f6c/ioqueue/test/ioq_udp.c#L535
+  https://github.com/gnolizuh/Real-Time-Monitor-agent/blob/9c179ef76526f0ddd04c00f8eafd6e5421b1b7d4/Monitor/Monitor/Com.cpp
+
+  https://www.pjsip.org/pjlib/docs/html/group__PJ__ACTIVESOCK.htm#ga2374729a4261eb7a1e780110bcef2e37
+  https://www.pjsip.org/pjlib/docs/html/structpj__activesock__cb.htm
+*/
+
+
+static pj_bool_t on_data_read(pj_activesock_t *asock, void *data, pj_size_t size, pj_status_t status, pj_size_t *remainder);
+static pj_bool_t on_data_sent(pj_activesock_t *asock, pj_ioqueue_op_key_t *op_key, pj_ssize_t sent);
+static pj_bool_t on_accept_complete(pj_activesock_t *asock, pj_sock_t newsock, const pj_sockaddr_t *src_addr, int src_addr_len);
+static pj_bool_t on_connect_complete(pj_activesock_t *asock, pj_status_t status);
+
+static pj_activesock_cb activesock_cb = {
+    &on_data_read,
+    NULL,
+    &on_data_sent,
+    &on_accept_complete,
+    NULL,
+    &on_connect_complete
+};
+
+
+static pj_bool_t on_data_read(pj_activesock_t *asock, void *data, pj_size_t size, pj_status_t status, pj_size_t *remainder) {
+    printf("on_data_read\n");
+    printf("%.*s\n", size, data);
+    if(size == 0) {
+        // TODO: destroy the activesock.
+        return PJ_FALSE;
+    }
+    return PJ_TRUE;
+}
+
+static pj_bool_t on_data_sent(pj_activesock_t *asock, pj_ioqueue_op_key_t *op_key, pj_ssize_t sent) {
+    printf("on_data_sent\n");
+    return PJ_TRUE;
+}
+
+static pj_bool_t on_accept_complete(pj_activesock_t *asock, pj_sock_t newsock, const pj_sockaddr_t *src_addr, int src_addr_len) {
+    printf("on_accept_complete\n");
+
+    pj_activesock_t *new_asock = NULL;
+
+    pj_ioqueue_t *ioqueue = (pj_ioqueue_t*)pj_activesock_get_user_data(asock);
+
+    pj_status_t rc = pj_activesock_create(g_pool, newsock, pj_SOCK_STREAM(), NULL, ioqueue, &activesock_cb, NULL, &new_asock);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_create for newsock failed %d\n", rc);
+        return PJ_FALSE;
+    }
+
+    rc = pj_activesock_set_user_data(new_asock, ioqueue);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_set_user_data failed %d\n", rc);
+        return PJ_FALSE;
+    }
+
+    rc = pj_activesock_start_read(new_asock, g_pool, 1000, 0);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_start_read() failed with %d\n", rc);
+        return PJ_FALSE;
+    }
+    printf("pj_activesock_start_read() success\n");
+    return PJ_TRUE;
+}
+
+static pj_bool_t on_connect_complete(pj_activesock_t *asock, pj_status_t status) {
+    printf("on_connect_complete\n");
+    return PJ_TRUE;
+}
+
+static int create_tcp_socket(int port, pj_pool_t *pool) {
+    pj_ioqueue_key_t **skey;
+    pj_ioqueue_t *ioqueue = pjsip_endpt_get_ioqueue(g_sip_endpt);
+
+    skey = (pj_ioqueue_key_t**)pj_pool_alloc(pool, sizeof(pj_ioqueue_key_t*));
+    pj_sock_t *sock = (pj_sock_t*)pj_pool_alloc(pool, sizeof(pj_sock_t));
+
+    pj_status_t rc;
+    pj_sockaddr_in addr, client_add, rmt_addr;
+    int client_addr_len;
+
+    pj_activesock_t *asock = NULL;
+
+    rc = pj_sock_socket(pj_AF_INET(), pj_SOCK_STREAM(), 0, sock);
+	if (rc != PJ_SUCCESS || *sock == PJ_INVALID_SOCKET) {
+	    printf("....unable to create socket, rc=%d\n", rc);
+        goto on_error;
+	}
+
+    // Bind server socket.
+    pj_sockaddr_in_init(& addr, 0, port);
+    if ((rc = pj_sock_bind(*sock, &addr, sizeof(addr))) != 0) {
+        printf("...bind error %d\n", rc);
+        goto on_error;
+    }
+
+    // Server socket listen().
+    if (pj_sock_listen(*sock, 5)) {
+        printf("...ERROR in pj_sock_listen() %d\n", rc);
+        goto on_error;
+    }
+
+    // We also need to follow https://www.pjsip.org/pjlib/docs/html/group__PJ__ACTIVESOCK.htm
+    // https://cpp.hotexamples.com/examples/-/-/pj_sockaddr_in_init/cpp-pj_sockaddr_in_init-function-examples.html
+
+    rc = pj_activesock_create(pool, *sock, pj_SOCK_STREAM(), NULL, ioqueue, &activesock_cb, NULL, &asock);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_create failed %d\n", rc);
+        goto on_error;
+    }
+
+    rc = pj_activesock_set_user_data(asock, ioqueue);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_set_user_data failed %d\n", rc);
+	    goto on_error;
+    }
+
+    rc = pj_activesock_start_accept (asock, pool);
+    if (rc != PJ_SUCCESS) {
+        printf("pj_activesock_start_accept failed %d\n", rc);
+	    goto on_error;
+    }
+
+    printf("create_tcp_socket success\n");
+    return 0;
+
+on_error:
+    printf("create_tcp_socket failure\n");
+    return -1;
+}
+
 int __pjw_init()
 {
 	addon_log(LOG_LEVEL_DEBUG, "pjw_init thread_id=%i\n", syscall(SYS_gettid));
@@ -920,6 +1057,8 @@ int __pjw_init()
 		addon_log(LOG_LEVEL_DEBUG, "start_digit_buffer_thread() failed\n");
 		return 1;
 	}
+
+    create_tcp_socket(9000, g_pool);
 
 	return 0;
 }
