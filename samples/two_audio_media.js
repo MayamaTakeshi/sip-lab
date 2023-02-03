a9b48ccd6fdaef615e2ed78a155abd9591dfd67a
MayamaTakeshi <mayamatakeshi@gmail.com>
2023-02-03 16:20:22 +0900

    Preserving work so far

--- samples/two_audio_media.js
+++ samples/two_audio_media.js
@@ -10,6 +10,14 @@ async function test() {
     //sip.set_log_level(6)
     sip.dtmf_aggregation_on(500)
 
+    // Let's ignore '100 Trying'
+    z.add_event_filter({
+        event: 'response',
+        msg: sip_msg({
+            $rs: '100',
+        }),
+    })
+
     z.trap_events(sip.event_source, 'event', (evt) => {
         var e = evt.args[0]
         return e
@@ -29,21 +37,6 @@ async function test() {
         {
             event: "incoming_call",
             call_id: m.collect("call_id"),
-        },
-        {
-            event: 'response',
-            call_id: oc.id,
-            method: 'INVITE',
-            msg: sip_msg({
-                $rs: '100',
-                $rr: 'Trying',
-                '$(hdrcnt(via))': 1,
-                '$hdr(call-id)': m.collect('sip_call_id'),
-                $fU: 'alice',
-                $fd: 'test.com',
-                $tU: 'bob',
-                '$hdr(l)': '0',
-            }),
         },
     ], 1000)
 
@@ -138,14 +131,6 @@ async function test() {
         {
             event: 'reinvite',
             call_id: ic.id,
-        },
-        {
-            event: 'response',
-            call_id: oc.id,
-            method: 'INVITE',
-            msg: sip_msg({
-                $rs: '100',
-            }),
         },
     ], 1000)
 
@@ -245,6 +230,236 @@ async function test() {
     ], 1000)
 
     await z.sleep(100)
+
+    // Now change to single media
+    sip.call.reinvite(oc.id, {media: "audio"})
+
+    await z.wait([
+        {
+            event: 'reinvite',
+            call_id: ic.id,
+        },
+    ], 1000)
+
+    sip.call.respond(ic.id, {code: 200, reason: 'OK', media: "audio,audio"})
+
+    await z.wait([
+        {
+            event: 'response',
+            call_id: oc.id,
+            method: 'INVITE',
+            msg: sip_msg({
+                $rs: '200',
+            }),
+        },
+        {
+            event: 'media_update',
+            call_id: ic.id,
+            status: 'ok',
+            media: [
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10004,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10000,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              },
+            ]
+        },
+        {
+            event: 'media_update',
+            call_id: oc.id,
+            status: 'ok',
+            media: [
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10000,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10004,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              },
+            ]
+        },
+    ], 1000)
+
+    sip.call.send_dtmf(oc.id, {digits: '1234', mode: 0})
+    sip.call.send_dtmf(ic.id, {digits: '4321', mode: 1})
+
+    await z.wait([
+        {
+            event: 'dtmf',
+            call_id: ic.id,
+            digits: '1234',
+            mode: 0,
+            media_id: 0
+        },
+        {
+            event: 'dtmf',
+            call_id: oc.id,
+            digits: '4321',
+            mode: 1,
+            media_id: 0
+        },
+    ], 1500)
+
+    // now switch back to two media
+    sip.call.reinvite(oc.id, {media: "audio,audio"})
+
+    await z.wait([
+        {
+            event: 'reinvite',
+            call_id: ic.id,
+        },
+    ], 1000)
+
+    sip.call.respond(ic.id, {code: 200, reason: 'OK', media: "audio,audio"})
+
+    await z.wait([
+        {
+            event: 'response',
+            call_id: oc.id,
+            method: 'INVITE',
+            msg: sip_msg({
+                $rs: '200',
+            }),
+        },
+        {
+            event: 'media_update',
+            call_id: ic.id,
+            status: 'ok',
+            media: [
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10004,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10000,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              },
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10006,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10002,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              }
+            ]
+        },
+        {
+            event: 'media_update',
+            call_id: oc.id,
+            status: 'ok',
+            media: [
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10000,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10004,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              },
+              {
+                type: 'audio',
+                local: {
+                  addr: '127.0.0.1',
+                  port: 10002,
+                  mode: 'sendrecv'
+                },
+                remote: {
+                  addr: '127.0.0.1',
+                  port: 10006,
+                  mode: 'sendrecv'
+                },
+                fmt: [
+                  '0 PCMU/8000',
+                  '120 telephone-event/8000'
+                ]
+              }
+            ]
+        },
+    ], 1000)
+
+    sip.call.send_dtmf(oc.id, {digits: '1234', mode: 0})
+    sip.call.send_dtmf(ic.id, {digits: '4321', mode: 1})
+
+    await z.wait([
+        {
+            event: 'dtmf',
+            call_id: ic.id,
+            digits: '1234',
+            mode: 0,
+            media_id: 0
+        },
+        {
+            event: 'dtmf',
+            call_id: oc.id,
+            digits: '4321',
+            mode: 1,
+            media_id: 0
+        },
+        {
+            event: 'dtmf',
+            call_id: ic.id,
+            digits: '1234',
+            mode: 0,
+            media_id: 1
+        },
+        {
+            event: 'dtmf',
+            call_id: oc.id,
+            digits: '4321',
+            mode: 1,
+            media_id: 1
+        },
+    ], 1500)
 
     sip.call.terminate(oc.id)
 
